3ec5a972e09b013c44dd367d6af3fd95bc9ec1ff
markus232 <markusbrll7@gmail.com>
2026-03-14 10:27:06 -0700

    Created scenes with basic scripts

---
+++ Mbrill_Hour7_P1/Assets/Scripts/HelloWorldScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+public class HelloWorldScript : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        print ("Hello World");
+    }
+
+    // Update is called one per frame
+    void Update()
+    {
+
+    }
+}
