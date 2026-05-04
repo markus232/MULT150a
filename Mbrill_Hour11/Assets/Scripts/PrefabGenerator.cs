39979bb718f634de39382ab6ec762a12fb2865d0
markus232 <markusbrll7@gmail.com>
2026-05-03 20:15:54 -0700

    Created a scene with a custom lamp prefab and a spawning script.

--- Mbrill_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ Mbrill_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public int numberOfLamps = 10;
+    public float spacing = 5.0f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numberOfLamps; i++)
+        {
+            // Spawns lamps in a line along the X-axis
+            Vector3 spawnPosition = new Vector3(i * spacing, 0, 0);
+            Instantiate(prefab, spawnPosition, Quaternion.identity);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Spawns at 0,0,0 (World Origin)
         if (Input.GetKeyDown(KeyCode.B))
         {
             Instantiate(prefab);
         }
 
+        // Spawns at the Generator's current position
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
