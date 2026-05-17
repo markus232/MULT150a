e1bba909e0d4e8f2b9b58d0c1f89673ab9532484
markus232 <markusbrll7@gmail.com>
2026-05-17 15:46:28 -0700

    Created a scene with a user interface and a start button.

---
+++ Mbrill_Hour14andHour16/Assets/UserInterface.cs
@@ -0,0 +1,7 @@
+/*
+1. Created and imported a custom background.
+2. Added a title, a greeting, and a stat display.
+3. Changed font color, size, style, gradient, and added a underlay.
+4. Added a highlighted color and pressed color to the button.
+5. Reduced the softness of Face and increased the dilate.
+*/
