7c5d21944f13054a49137514059c44ae519106d5
markus232 <markusbrll7@gmail.com>
2026-05-17 17:36:49 -0700

    Created a scene with a user interface, a start button, and a particle effect.

---
+++ Mbrill_Hour14andHour16/Assets/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+1. Changed color to blue.
+2. Added a color over lifetime from dark blue to light blue to white.
+3. Added noise to make particles dance a bit more.
+4. Changed shape to box and increase values on randomize direction, spherize direction, and randomize position.
+5. Reduced start speed to 1 and max particles to 100.
+6. Added curve to start size alternating in quick successions of large and small particles.
+*/
