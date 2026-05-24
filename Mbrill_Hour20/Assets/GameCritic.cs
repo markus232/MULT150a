8559f56c442c03a589b2d94c30ab66d84dfe8f2b
markus232 <markusbrll7@gmail.com>
2026-05-24 14:54:23 -0700

    Created a game with ppower-upsand obstacles.

---
+++ Mbrill_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,21 @@
+/*
+1. Is the game too easy or hard?
+The game is hard. The hit box on power ups is tricky. Phase protects from obstacles but also removes ability to hit power ups.
+
+2. What changes would you do to make it easier or harder?
+I would add more obstacles of different varieties and gradually increase the speed to make the game harder/fun. Allowing player to turn off phase would make game easier. Giving the player a way to destroy obstacles would help.
+
+3. What would make the game more appealing?
+Obstacles that you have to jump or roll under. Different power ups. Boss level where a flying boss hovers in front of you and launches projectiles and drops obstacles that the player has to dodge while trying to pick up a special power up that does damage to the boss.
+
+4. What parts of the game are fun? 
+The character movement feels smooth. The theme and visuals are exciting. It feels sastisfying running through obstacles using the phase power up.
+
+5. What parts of the game are tedious?
+Not being able to turn phase off to pick up power ups. There is no display of how much power I have left. adding a dash left or right ability would help grabbing the power ups in slipperly situations.
+
+Obstacle changes: Changed shape to look like a virus. Created a custom texture in paint. Added green emission to material
+
+Power up changes: Made shape look like a canister of phase particles contained behind glass. Custom phase fluid texture and canister metal texture made in paint. Added light blue particle system.
+
+*/
